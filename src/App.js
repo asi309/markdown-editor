84da277db4c7cac9a80f1a79f412ff9db366b6b0
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2020-10-06 16:59:14 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import marked from 'marked';
+
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const [markdown, setMarkdown] = useState('');
+
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+  });
+
+  useEffect(() => {
+    setText(text.replace(/\n/, '<br />'));
+    setMarkdown(marked(text));
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <h1>Markdown Editor</h1>
+      <div>
+        <section>
+          <h2>Editor</h2>
+          <textarea
+            id="editor"
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+        </section>
+        <section>
+          <h2>Previewer</h2>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: markdown }}
+          ></div>
+        </section>
+      </div>
+      <footer>&copy; Asidipta Chaudhuri</footer>
     </div>
   );
 }
